fd1e545dd0f47acd90a73852c9844b9436d036c8
«Adilya-Batyrbekova» <adilja2001batyrbekova@gmail.com>
2024-10-08 20:10:06 +0300

    Создание страниц, разбиение на компоненты, App

---
+++ src/components/App/app.tsx
@@ -0,0 +1,11 @@
+// src/components/App/app.tsx
+import React from 'react';
+import MainPage from '../../pages/Main/main';
+
+const App: React.FC = () => (
+  <div>
+    <MainPage />
+  </div>
+);
+
+export default App;

bca4a5adacece0ae3ab044845f4b4c024ccf0188
Adilya-Batyrbekova <adilja2001batyrbekova@gmail.com>
2024-11-04 20:50:22 +0300

    page 404

---
+++ src/pages/Eror/404.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+//import styles from './styles.module.css';
+
+function Error404(): JSX.Element {
+  return (
+    <div>
+          404 Not Found
+       <Link to="/" className="link_not_found">Go back to Home</Link>
+    </div>
+  );
+}
+
+export default Error404;
